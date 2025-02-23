8d8e37456f186848ac7217bab0f7a66bbf4f7fe4
gauravdwivedE <168353151+gauravdwivedE@users.noreply.github.com>
2025-02-24 02:40:47 +0530

    Update mongoDB.js

--- configs/mongoDB.js
+++ configs/mongoDB.js
@@ -1,14 +1,21 @@
-const mongoose = require('mongoose')
-const chalk = require('chalk')
+const mongoose = require('mongoose');
+const chalk = require('chalk');
 
 async function connectDB() {
+  // Check if the environment variable is loaded correctly
+  console.log('MONGO_DB_URI:', process.env.MONGO_DB_URI); // Log the URI for debugging
+  
+  if (!process.env.MONGO_DB_URI) {
+    console.log(chalk.red('MongoDB URI is not defined! Please check your environment variables.'));
+    return;
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_DB_URI)
+    await mongoose.connect(process.env.MONGO_DB_URI);
     console.log(chalk.green("MongoDBConnection: Connected to the database"));
-    
   } catch (err) {
     console.log(chalk.red(err));
   }
 }
 
-module.exports = connectDB
+module.exports = connectDB;
